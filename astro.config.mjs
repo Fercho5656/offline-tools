17a197f8b02dd1452f53b47e3e357136973e5ff0
Fernando Moreno <morenomoranfernando@gmail.com>
2025-01-22 01:30:23 -0600

    Install image encoders/decoders

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,10 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  vite: {
+    optimizeDeps: {
+      exclude: ['@jsquash/avif', '@jsquash/jpeg', '@jsquash/jxl', '@jsquash/png', '@jsquash/webp']
+    }
+  }
 });
